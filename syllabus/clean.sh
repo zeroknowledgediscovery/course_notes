dee5092ada747e3734059271b324d2581597bc2a
ishanu chattopadhyay <ishanu.chattopadhyay@gmail.com>
2021-01-10 15:27:10 -0600

    commit

---
+++ syllabus/clean.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm *.lot
+git rm *.lot
+
+rm *.lof
+git rm *.lof
+
+rm *.blg
+git rm *.blg
+
+rm *out
+git rm *out
+
+rm *fls
+git rm *fls
+
+git rm *~
+git rm *out
+git rm *log
+
+rm *~
+rm *out
+rm *log
+rm *latexmk
+rm *bak
+
+git rm *bak
+rm *aux*
+git rm *aux*
+rm *~
+rm *out
+rm *log
+git rm *fls
+git rm *aux
+git rm *auxlock
+git rm *latexmk
